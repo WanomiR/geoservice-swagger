78609ae69f02b5f991d5d33427b88df8f8822b02
Wanomir <ioan.romadin@gmail.com>
2024-07-14 14:48:06 +0300

    refactor: clean main

--- proxy/cmd/main.go
+++ proxy/cmd/main.go
@@ -24,43 +24,4 @@ func main() {
 	}
 
 	log.Fatal(a.Run())
-
-	//err := godotenv.Load()
-	//
-	//if err != nil {
-	//	log.Fatal("Error loading .env file", err)
-	//}
-	//
-	//jwtSecret := os.Getenv("JWT_SECRET")
-	//apiKey := os.Getenv("DADATA_API_KEY")
-	//secretKey := os.Getenv("DADATA_SECRET_KEY")
-	//port := os.Getenv("PORT")
-	//
-	//g := geoservice.NewGeoService(apiKey, secretKey)
-	//a := auth.NewUserAuth("HS256", jwtSecret)
-	//
-	//r := chi.NewRouter()
-	//
-	//proxy := reverse.NewReverseProxy("hugo", "1313")
-	//r.Use(proxy.ReverseProxy)
-	//
-	//r.Route("/api", func(r chi.Router) {
-	//	r.Post("/register", a.RegisterUser)
-	//	r.Post("/login", a.Authenticate)
-	//	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-	//		w.WriteHeader(200)
-	//		w.Write([]byte("Hello from API"))
-	//	})
-	//
-	//	r.Route("/address", func(r chi.Router) {
-	//		r.Use(a.RequireAuthentication)
-	//		r.Post("/search", g.HandleAddressSearch)
-	//		r.Post("/geocode", g.HandleAddressGeocode)
-	//	})
-	//})
-	//
-	//r.Get("/swagger/*", httpSwagger.Handler(
-	//	httpSwagger.URL("http://localhost"+port+"/swagger/doc.json"),
-	//))
-
 }
