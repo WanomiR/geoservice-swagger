69b9c8e85095c55483944e916032be025a72c488
Ivan Romadin <ioan.romadin@gmail.com>
2024-07-17 12:13:38 +0300

    Delete proxy/internal/entities directory

--- proxy/internal/entities/user.go
+++
@@ -1,6 +0,0 @@
-package entities
-
-type User struct {
-	Email    string `json:"email" binding:"required" example:"admin@example.com"`
-	Password string `json:"password" binding:"required" example:"password"`
-}
